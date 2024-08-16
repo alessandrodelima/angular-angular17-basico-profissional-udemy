ea26a62a5590989e08965626d7a63857f6447e26
alessandrolima <alessandrolima@cnj.jus.br>
2024-08-16 15:28:08 -0300

    Encadeamento Opcional na interpolacao

--- secao03-fundamentos-do-angular-componentes-estilizacoes/projeto-components/src/app/card/card.component.ts
+++ secao03-fundamentos-do-angular-componentes-estilizacoes/projeto-components/src/app/card/card.component.ts
@@ -1,24 +1,23 @@
 import { Component } from '@angular/core';
 
+interface IPlano {
+  infos: IInfos;
+}
+
+interface IInfos {
+  tipo: string;
+  preco: number;
+}
+
 @Component({
   selector: 'app-card',
   templateUrl: './card.component.html',
   styleUrl: './card.component.scss'
 })
+
 export class CardComponent {
-/*
-  tipo = 'Teste';
-  preco = 100;
-
-  public getFullPrice() {
-    setTimeout(() => {
-      console.log('Set timeout');
-      this.tipo = 'Simples';
-    }, 4000);
-    return 'R$' + this.preco + ',00/Mês';
-  }*/
-
-  plano = {
+  // @ts-ignore
+  plano: IPlano = {
     infos: {
       tipo: 'Simples',
       preco: 100,
